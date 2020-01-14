0479e4193b7fd4d96743043faa9ee70c310fc414
ViniciusNyari <viniciusnyari@gmail.com>
2020-01-14 15:50:14 -0300

    Adicionando express

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+/*
+Rotas
+*/
+
+const express = require('express');
+
+const app = express();
+
+app.get('/',(request, response)=> {
+    return response.json({message: 'Hello Omnistack'});
+});
+
+app.listen(3333);
+
