f31baea257cfedb30ccef01a3899b42cdd42e4c5
ViniciusNyari <viniciusnyari@gmail.com>
2020-01-16 11:36:29 -0300

    15/01 - Construindo a interface web

--- backend/src/index.js
+++ backend/src/index.js
@@ -9,6 +9,9 @@ const mongoose = require('mongoose');
 //Importando as rotas
 const routes = require('./routes')
 
+//Importando cors - permite que o backend seja acessado pelo front - padrão não consegue
+const cors = require('cors');
+
 //declara uma instancia de express
 const app = express();
 
@@ -18,11 +21,16 @@ mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-v120u.mongodb.net/w
     useUnifiedTopology: true
 });
 
+
+//Libera acesso para toda aplicação que tentar se conectar a ela
+app.use(cors());
+
 //Configurando para o express usar json
 app.use(express.json());
 
 //Usando as rotas
 app.use(routes);
+
 
 //Informa que a nossa aplicaçao responderá na porta 3333
 app.listen(3333);
--- web/src/App.js
+++ web/src/App.js
@@ -1,5 +1,12 @@
-import React, {useState} from 'react';
-import Header from './Header';
+import React, {useState,useEffect} from 'react';
+import './global.css';
+import './App.css';
+import './Sidebar.css';
+import './Main.css';
+import api from './Service/api';
+import DevItem from './components/DevItem';
+import DevForm from './components/DevForm';
+
 
 //Conceitos do React
 //Componente: bloco isolado de HTML, CSS e JS, o qual não interfere no restante da aplicação
@@ -7,10 +14,45 @@ import Header from './Header';
 //Propriedade: informações que um componente pai passa para os componentes filhos
 
 function App() {
+
+  const [devs, setDevs] = useState([]);
+  
+  useEffect(()=> {
+
+    async function loadDevs() {
+      const response = await api.get('/devs');
+      setDevs(response.data);
+    }
+
+    loadDevs();
+
+  },[]);
+
+  async function handleAddDev(data){
+      const response = await api.post('/devs',data); 
+      
+      //Adiciona a lista atual e mais o retorno do método
+      setDevs([...devs,response.data]);
+  }
+
   return (
-        <>      
-          <Header/>      
-      </>
+    <div id="app">
+      <aside>
+          <strong> Cadastrar</strong>
+          <DevForm onSubmit={handleAddDev}/>
+      </aside>
+      <main>
+        <ul>
+        
+          {devs.map(dev => (
+            <DevItem key={dev._id} dev={dev}/>
+          ))}
+
+        </ul>
+      </main>
+    </div>
+
+       
     );  
 }
 
